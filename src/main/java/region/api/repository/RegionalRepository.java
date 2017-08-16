7701123b6903c9585912eba02257bd9b05efa1b0
rusman <rusman.jay@gmail.com>
2017-08-16 11:45:59 +0700

    Remove regional view (mysql view performance issues)

--- src/main/java/region/api/repository/RegionalRepository.java
+++
@@ -1,9 +0,0 @@
-package region.api.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import region.api.models.Regional;
-
-public interface RegionalRepository extends CrudRepository<Regional, Long> {
-
-}
